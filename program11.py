cbbbb3c82bcc7b4880e40883f1971865f92dbec7
ArmaanGuru <139745380+ArmaanGuru@users.noreply.github.com>
2025-12-10 20:21:23 -0800

    Create program11.py
    problem11

---
+++ program11.py
@@ -0,0 +1,8 @@
+# program to check leap year
+year =int(input('enter a year:'))
+if year%400==0:
+    print(f'{year} is a leap year')
+elif(year %4==0) and (year%100!=0):
+    print(f'{year} is a leap year')
+else:
+    print(f'{year} is not a leap year')
